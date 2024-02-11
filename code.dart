200930c5028d32e1218210a1813b6ff548d3758e
Muhammad Saeed Jangda <muhammadsaeedjangda@gmail.com>
2024-02-12 02:07:58 +0500

    Update code.dart

--- code.dart
+++ code.dart
@@ -5,11 +5,46 @@
   print("Output:");
   for (int i = 0; i < numbers.length; i++) {
     if (numbers[i] % 2 == 0) {
-      // Check if the number is even
       print(numbers[i]);
     }
   }
 }*/
 
 //Q.02
+/*void main() {
+  int limit = 25;
+  print("Input: $limit");
+  print("Output:");
+  int first = 0;
+  int second = 1;
+  for (int i = 0; first <= limit; i++) {
+    print(first);
+    int next = first + second;
+    first = second;
+    second = next;
+  }
+}*/
+
+//Q.03
+/*void main() {
+  int number = 17; 
+  print("Input: $number");
+  if (isPrime(number)) {
+    print("$number is a prime number.");
+  } else {
+    print("$number is not a prime number.");
+  }
+}
+bool isPrime(int n) {
+  if (n <= 1) {
+    return false;
+  }
+  for (int i = 2; i <= n / 2; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}*/
+
 
