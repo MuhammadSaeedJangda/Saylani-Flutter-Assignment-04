e4ff635d0f713f04840e2a87f95ebca7c988ee84
Muhammad Saeed Jangda <muhammadsaeedjangda@gmail.com>
2024-02-12 02:02:53 +0500

    Create code.dart

---
+++ code.dart
@@ -0,0 +1,15 @@
+//Q.01
+/*void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  print("Input: $numbers");
+  print("Output:");
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) {
+      // Check if the number is even
+      print(numbers[i]);
+    }
+  }
+}*/
+
+//Q.02
+
